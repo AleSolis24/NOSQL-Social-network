29bbfa4840f473c26aec440946eb8ebeed5b8fae
AleSolis24 <aletitan24@icloud.com>
2024-01-27 23:51:04 -0500

    finshed adding thoughts.js by adding the way the user is inputting there reaction and when the reaction started as well as putting the reaction body

--- models/thoughts.js
+++ models/thoughts.js
@@ -3,6 +3,37 @@ const mongoose = require('mongoose');
 const userThoughts = new mongoose.Schema({
     thoughtMessage: {
         type: String,
+        required: true,
+        minlength: 10,
+        maxlenght: 100
+    },
+
+    createdAt: {
+        type: Data,
+        default: Data.now,
+    },
+
+    username: {
+        type: String,
         required: true
-    }
-})
+    },
+
+    reactions: [
+        {
+            reactionBody: {
+                type: String,
+                required: true,
+                maxlength: 100
+            },
+            username: {
+                type: String,
+                required: true
+            },
+
+        },
+    ],
+});
+
+const Thoughts = mongoose.model('Thoughts', userThoughts);
+
+module.exports = Thoughts;
