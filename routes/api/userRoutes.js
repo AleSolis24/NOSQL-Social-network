be80d2eb456827633e88b6d6b41328154fa6b6e9
AleSolis24 <aletitan24@icloud.com>
2024-01-28 00:56:20 -0500

    added userRoutes.js to get a certain api for getting all user/make a new account/ find certain accounts/ update your account/ delete account/ and add new friend or delete friend

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {
+    getAllAccounts,
+    getAccountById,
+    newAccount,
+    deleteAccount,
+    updateAccount,
+    newFriend,
+    deleteFriend
+} = require('../../controllers/userthought');
+
+// Route to get all accounts and also to get new acocunt for the api
+router.route('/users')
+.get(getAllAccounts)
+.get(newAccount);
+
+// Route to find a certain user ID and to update user account and delete there acount
+router.route('./users/:userId')
+.get(getAccountById)
+.get(updateAccount)
+.get(deleteAccount)
+
+// Route to get a new friend or delete a fake friend 
+router.route('./users/:userId/friends/:friendsId')
+.get(newFriend)
+.get(deleteFriend)
+
+
+
+module.exports = router;
